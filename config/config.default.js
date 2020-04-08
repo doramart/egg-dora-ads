4118d2b6dc199a8fab8d6799cf9758148f2796a0
doramart <doramart@qq.com>
2020-04-08 12:43:24 +0800

    update egg-dora-ads

--- config/config.default.js
+++ config/config.default.js
@@ -17,7 +17,7 @@ exports.doraAds = {
     isindex: 0, // 是否需要前台访问，1：需要，0：不需要,入口地址:'/ext/devteam/index/index'
     version: pkgInfo.version, // 版本号
     iconName: 'icon_shakehands_fill', // 主菜单图标名称
-    adminUrl: 'https://cdn.html-js.cn/cms/plugins/static/admin/ads/js/app.js',
+    adminUrl: '/ads/js/app.js',
     adminApi: [{
         url: 'ads/getList',
         method: 'get',
@@ -53,8 +53,7 @@ exports.doraAds = {
     initData: '', // 初始化数据脚本
     pluginsConfig: ` 
     exports.doraAds = {\n
-        enable: true,\n
-        \n
+        enable: true,\n        package: 'egg-dora-ads',    
     };\n
     `, // 插入到 plugins.js 中的配置
     defaultConfig: `
